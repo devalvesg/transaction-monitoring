9351c0f4b34e1e250353ffc5a7f2290782014737
Gabriel Alves <gabrieldamasceno881@outlook.com>
2025-10-22 21:43:18 -0300

    feat: creating project structure

---
+++ fraud-analysis-service/src/main/java/com/devalvesg/fraud_analysis_service/FraudAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devalvesg.fraud_analysis_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudAnalysisServiceApplication.class, args);
+	}
+
+}
---
+++ fraud-analysis-service/src/test/java/com/devalvesg/fraud_analysis_service/FraudAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devalvesg.fraud_analysis_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudAnalysisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
